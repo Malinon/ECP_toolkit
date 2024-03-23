6c6d55bd0654e7822d1dfc0564584052f0b1a379
Jakub Malinowski <jakubbmalinowski@gmail.com>
2024-03-23 21:34:04 +0100

    Remove print from sample app

--- sample/ecpFromPerseus.jl
+++ sample/ecpFromPerseus.jl
@@ -6,7 +6,6 @@ input_data = Ecp.read_input_from_perseus_file(ARGS[1])
 # Parse number of steps for each filtration parameter
 number_of_steps = Tuple(parse(Int64, ARGS[i]) for i in 3:length(ARGS))
 
-println(input_data, number_of_steps)
 # Compute and vectorize ECP
 contributions = length(size(input_data)) == 2 ? Ecp.compute_contributions_2d(input_data) : Ecp.compute_contributions_3d(input_data)
 vectorised_ecp = Ecp.vectorize_ecp(contributions, number_of_steps)
